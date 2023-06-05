bd577f873035c7c8708fc617f095ef02fc785a2b
hein.66 <hein.66@osu.edu>
2023-06-05 11:08:17 -0400

    Renamed to tensorflow demo

--- tensorflow-demo.py
+++ tensorflow-demo.py
@@ -78,56 +78,56 @@ if __name__ == '__main__':
         'Ankle boot'
     ]
 
-    # # Show example image
-    # show_image(train_images[0])
-    #
-    # # Scale images down
-    # train_images = train_images / 255.0
-    # test_images = test_images / 255.0
-    #
-    # # Show 25 images
-    # show_images(train_images, train_labels)
-    #
-    # # Create the model
-    # model = tf.keras.Sequential([
-    #     tf.keras.layers.Flatten(input_shape=(28, 28)),
-    #     tf.keras.layers.Dense(128, activation='relu'),
-    #     tf.keras.layers.Dense(10)
-    # ])
-    # model.compile(optimizer='adam',
-    #               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    #               metrics=['accuracy'])
-    #
-    # # Train the model
-    # model.fit(train_images, train_labels, epochs=10)
-    #
-    # test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
-    # print('\nTest accuracy:', test_acc)
-    #
-    # probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
-    # predictions = probability_model.predict(test_images)
-    #
-    # num_rows = 5
-    # num_cols = 3
-    # num_images = num_rows * num_cols
-    # plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
-    # for i in range(num_images):
-    #     plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
-    #     plot_image(i, predictions[i], test_labels, test_images)
-    #     plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
-    #     plot_value_array(i, predictions[i], test_labels)
-    # plt.tight_layout()
-    # plt.show()
-    #
-    # img = test_images[1]
-    # print(img.shape)
-    #
-    # img = (np.expand_dims(img, 0))
-    # print(img.shape)
-    #
-    # predictions_single = probability_model.predict(img)
-    # print(predictions_single)
-    #
-    # plot_value_array(1, predictions_single[0], test_labels)
-    # _ = plt.xticks(range(10), class_names, rotation=45)
-    # plt.show()
+    # Show example image
+    show_image(train_images[0])
+
+    # Scale images down
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
+
+    # Show 25 images
+    show_images(train_images, train_labels)
+
+    # Create the model
+    model = tf.keras.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(10)
+    ])
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    # Train the model
+    model.fit(train_images, train_labels, epochs=10)
+
+    test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+    print('\nTest accuracy:', test_acc)
+
+    probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+    predictions = probability_model.predict(test_images)
+
+    num_rows = 5
+    num_cols = 3
+    num_images = num_rows * num_cols
+    plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
+    for i in range(num_images):
+        plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
+        plot_image(i, predictions[i], test_labels, test_images)
+        plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
+        plot_value_array(i, predictions[i], test_labels)
+    plt.tight_layout()
+    plt.show()
+
+    img = test_images[1]
+    print(img.shape)
+
+    img = (np.expand_dims(img, 0))
+    print(img.shape)
+
+    predictions_single = probability_model.predict(img)
+    print(predictions_single)
+
+    plot_value_array(1, predictions_single[0], test_labels)
+    _ = plt.xticks(range(10), class_names, rotation=45)
+    plt.show()
